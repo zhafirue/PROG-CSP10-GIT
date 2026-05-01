1b80b132a529b272735d92888fabf95cd4718926
aadzr <zhafiradzra@gmail.com>
2026-05-01 11:10:54 +0700

    Update by Gabriella

--- main.c
+++ main.c
@@ -21,8 +21,9 @@ void vigenereDecrypt(char *ciphertext, char *key) {
 }
 
 int main() {
-    char ciphertext[] = "!gsaH xinH ,fmaskgW rudT"; //Pesan Enkripsi
-    char key[] = "tempkey";
+    char ciphertext[] = "Asiy Nmezpam, Ytce Wozx!"; //Pesan Enkripsi
+    char key[] = "RealKey";
+
 
     printf("Encypted Text : %s\n", ciphertext);
     vigenereDecrypt(ciphertext, key);
